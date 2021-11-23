d12456ba21e597428d04a173ec78da0211af2d76
hsimao <e087754958@gmail.com>
2021-11-23 21:07:17 +0800

    feat: handle matched logic

---
+++ screens/MatchedScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MatchedScreen = () => {
+  return (
+    <View>
+      <Text>Mateched</Text>
+    </View>
+  );
+};
+
+export default MatchedScreen;
